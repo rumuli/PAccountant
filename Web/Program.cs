0c2e3eec3fe55ccc330d7118c469f193517839cd
= <=>
2026-02-19 10:22:11 +0200

    Added git configuration and initialized PAcoountant project

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,55 +1,17 @@
 using Web.Components;
-using Application.Services.Customers;
-using Application.Services.Campaigns;
-using Application.Services.SupportTickets;
-using Infrastructure.DependencyInjection;
-using Application.Services.Leads;
-using Application.Services.Deals;
-using Application.Services.Notes;
-using Application.Services.Activities;
 using MudBlazor.Services;
-using Application.Services.Users;
-using Application.Interfaces.Users;
-
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddMudServices();//mudblazor services for UI components
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-// Add HttpClient for API calls
-    builder.Services.AddHttpClient();
 
-    builder.Services.AddMudServices();
-
-
-   
-
-    //dependency injections for infrastructure layer
-
-    builder.Services.AddInfrastructureServices(builder.Configuration);
-
-    
-    //dependency injections for application layer   
-
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-builder.Services.AddScoped<ICampaignService, CampaignService>();
-builder.Services.AddScoped<ISupportTicketService, SupportTicketService>();    
-builder.Services.AddScoped<IDealService, DealService>(); 
-builder.Services.AddScoped<ILeadService, LeadService>();
-builder.Services.AddScoped<INoteService, NoteService>();
-builder.Services.AddScoped<IActivityService, ActivityService>();
-builder.Services.AddScoped<IIdentityService, IdentityService>();
-
-
-
-
-var app = builder.Build(); 
-
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -61,18 +23,11 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
-//Add authentication and authorization middleware
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 
 app.Run();
 
--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59e65c0f2b2c8f03b6f7395ced1e7300a956ee6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
