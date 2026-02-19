6736cc0ea10a9c37eb92bae4d26290e90e34e9d8
Clarisse88-creator <nyiraminaniclarisse88@gmail.com>
2026-02-19 14:45:41 +0200

    Update only AccountType.cs

---
+++ Core/Domain/Entities/AccountType.cs
@@ -0,0 +1,12 @@
+namespace Domain.Entities
+{
+    public class AccountType
+    {
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public string Status {get;set;}
+        public string CreatedBy {get;set;}
+        public string CreatedAt {get;set;}
+        public string UpdateBy{get;set;}
+    }
+}
