d109f00170c3d96fc6d929095486969360d930e8
= <=>
2026-02-20 15:13:29 +0200

    Add Lend entity

--- Infrastructure/obj/Debug/net10.0/Infrastucture.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastucture.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastucture")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1794fa8d752e978391f0cdfd3231834c897ae9a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf84b4ab20ce93b0224574b85173f2e71e6faa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastucture")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastucture")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
