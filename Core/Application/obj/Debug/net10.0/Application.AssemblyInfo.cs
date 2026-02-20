b297456e7f6219f10943c09b0710685e22b42494
= <=>
2026-02-20 11:58:36 +0200

    saved field

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d682ea524fd977be1a47c8bf46831302c242c377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf3763f9c96482cb1ddca22164ef6091e6c76632")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
