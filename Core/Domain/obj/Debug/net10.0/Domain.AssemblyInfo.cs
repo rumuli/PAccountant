b55881081399a51f728cf4c214122cd5b9a81e28
=Garou Simeon <meong@ADMINGAROU>
2026-02-26 09:59:35 +0200

    Fixed the conflicts resulting from build files

--- Core/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Core/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536c5b287cc92d42b529857388db1291e067b9e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec55ca28a06ac2de461154f11f98db63c6711ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
