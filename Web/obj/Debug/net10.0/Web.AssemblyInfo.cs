0bf9a3c9bb542089cc2c215fb443b2262bc77ead
= <=>
2026-03-02 12:51:19 +0200

    all changes commited

--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfeca71962bb62bb84af4f8ad2f09fdb4ee602b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df2365137ac62966013ee8f5c3f6b95ba8ddcb56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
