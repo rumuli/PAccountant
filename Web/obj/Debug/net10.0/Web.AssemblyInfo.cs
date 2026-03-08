89017b1a8025cd4da09175afbecfb8b9e6e9a6f2
=Garou Simeon <meong@ADMINGAROU>
2026-03-08 11:03:35 +0200

    merged main

--- Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
+++ Web/obj/Debug/net10.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e13578cf75d64aea30c0b4598592df255b1e69d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d30e92c2f27933b1be64c611d16de7611bebc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
