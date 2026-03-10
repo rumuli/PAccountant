61942d7e50d26f11e738c2e9c72700269adb605b
=Garou Simeon <meong@ADMINGAROU>
2026-03-10 17:11:45 +0200

    ruyet

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb0d46271211e0765bc5caeada523590d95cc46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+302c7f9f58683b0e6ecfd2deba0ef0b61f93a854")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
