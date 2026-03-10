88a00792c2dec47d1d70d3909108c65156378f4d
= <=>
2026-03-10 14:01:33 +0200

    saving data

--- Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
+++ Core/Application/obj/Debug/net10.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14e31af67678c01f5661a4abe43fff497641bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50918137e3e0233a0b22d93fbd95fd054579ce42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
